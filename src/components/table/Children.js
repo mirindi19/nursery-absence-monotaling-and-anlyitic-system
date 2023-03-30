a3f2e89b1d5ab316238ab87acfda5f27c57ebf53
mahame Alfred <mahamealfred@gmail.com>
2023-03-30 14:16:12 +0200

    y5453

--- src/components/table/Children.js
+++ src/components/table/Children.js
@@ -18,14 +18,68 @@ import DialogTitle from '@mui/material/DialogTitle';
 import { useDispatch, useSelector } from "react-redux";
 import { getStudentsAction } from "../../redux/actions/getStudentsAction";
 import { getClassesAction } from "../../redux/actions/getClassesAction";
-import { MenuItem } from "@mui/material";
+import { Alert, Collapse, IconButton, MenuItem } from "@mui/material";
+import { addStudentAction } from "../../redux/actions/addStudentAction";
+import { Close } from "@mui/icons-material";
 const Children = () => {
   const [open, setOpen] = React.useState(false);
   const dispatch=useDispatch();
-  const getStudents=useSelector((state)=>state.getStudents)
-  const getClasses=useSelector((state)=>state.getClasses)
+  const getStudents=useSelector((state)=>state.getStudents);
+  const getClasses=useSelector((state)=>state.getClasses);
+  const addStudent=useSelector((state)=>state.addStudent);
+
   const [studentsList,setStudentsList]=React.useState([])
   const [classesList,setClassesList]=React.useState([])
+  const [firstName,setFirstName]=React.useState("");
+  const [lastName,setLastName]=React.useState("");
+  const [dob,setDob]=React.useState("");
+  const [parentId,setParentId]=React.useState("");
+  const [classId,setClassId]=React.useState("");
+
+  const [firstNameError,setFirstNameError]=React.useState("");
+  const [lastNameError,setLastNameError]=React.useState("");
+  const [dobError,setDobError]=React.useState("");
+  const [parentIdError,setParentIdError]=React.useState("");
+  const [classIdError,setClassIdError]=React.useState("");
+
+  const [openError, setOpenError] = React.useState(true);
+  const [openSuccess, setOpenSuccess] = React.useState(true);
+  const [successMessage,setSuccessMessage]=React.useState("");
+
+
+  const handleCloseMessage=()=>{
+    setOpenError(false)
+    setOpenSuccess(false)
+      }
+    
+  const handleSubmit=async (e)=>{
+    e.preventDefault()
+if(firstName==""){
+setFirstNameError("First Name is required");
+}
+else if(lastName==""){
+ setLastNameError("Lass Name is required");
+}
+else if(dob==""){
+  setDobError("Select Date of Birth");
+ }
+ else if(parentId==""){
+  setParentIdError("Parent Phone Number is required");
+ }
+ else if(classId==""){
+  setClassIdError("Select Class Name");
+ }
+else{
+  setFirstNameError("");
+  setLastNameError("");
+  setDobError("");
+  setParentIdError("");
+  setClassIdError("");
+ await dispatch(addStudentAction(firstName,lastName,dob,parentId,classId))
+}
+ }
+
+
   React.useEffect(()=>{
    async function fetchData(){
     await dispatch(getClassesAction())
@@ -71,6 +125,25 @@ const Children = () => {
     fetchData()
    },[getStudents.details])
 
+
+   React.useEffect(()=>{
+    async function fetchData(){
+  if(!addStudent.loading){
+    if(addStudent.details.length!==0){
+      setSuccessMessage(addStudent.details.message)
+      setOpenSuccess(true)
+   setDob("")
+   setFirstName("")
+   setLastName("")
+   setClassId("")
+  setParentId("")
+      await dispatch(getStudentsAction())
+    }
+  }
+    }
+    fetchData()
+   },[addStudent.details])
+
   return (
     <div className='children'>
     <Sidebar/>
@@ -78,12 +151,58 @@ const Children = () => {
     <Navbar/>
     <div className="childrenTable">
     <Button variant="outlined" onClick={handleClickOpen}>
-    Add Children
+    Add New Student
   </Button>
   <Dialog open={open} onClose={handleClose}>
-    <DialogTitle>Registration</DialogTitle>
+    <DialogTitle>Add New Student</DialogTitle>
     <DialogContent>
+    {
+            !addStudent.error ? null : (
+                <Collapse in={openError}>
+                    <Alert severity="error"
+                        action={
+                            <IconButton
+                        aria-label="close"
+                        color="inherit"
+                        size="small"onClick={handleCloseMessage}>
+                        <Close
+                        fontSize="inherit"/></IconButton>
+                        }
+                        sx={
+                            {mb: 0.2}
+                    }>
+                        {/* {errorMessage==="Provided NID is not registered to the head of the household"?`${t("cbhi:providedNIDisnotregisteredtotheheadofthehousehold")}`:errorMessage} */}
+                        {addStudent.error}
+                         </Alert>
+                </Collapse>
+            )
+        }
+    {
+            !successMessage ? null : (
+                <Collapse in={openSuccess}>
+                    <Alert severity="success"
+                        action={
+                            <IconButton
+                        aria-label="close"
+                        color="inherit"
+                        size="small"onClick={handleCloseMessage}>
+                        <Close
+                        fontSize="inherit"/></IconButton>
+                        }
+                        sx={
+                            {mb: 0.2}
+                    }>
+                        {/* {errorMessage==="Provided NID is not registered to the head of the household"?`${t("cbhi:providedNIDisnotregisteredtotheheadofthehousehold")}`:errorMessage} */}
+                        {successMessage}
+                         </Alert>
+                </Collapse>
+            )
+        }
       <TextField
+      value={firstName}
+      onChange={(e)=>setFirstName(e.target.value)}
+      helperText={firstNameError? firstNameError : ""}
+      error={firstNameError}
       autoFocus
       margin="dense"
       id="firstName"
@@ -93,6 +212,10 @@ const Children = () => {
       variant="standard"
     />
     <TextField
+    value={lastName}
+    onChange={(e)=>setLastName(e.target.value)}
+    helperText={lastNameError? lastNameError : ""}
+    error={lastNameError}
     autoFocus
     margin="dense"
     id="lastName"
@@ -102,6 +225,10 @@ const Children = () => {
     variant="standard"
   />
   <TextField
+  value={dob}
+  onChange={(e)=>setDob(e.target.value)}
+  helperText={dobError? dobError : ""}
+  error={dobError}
   autoFocus
   margin="dense"
   id="dob"
@@ -110,15 +237,23 @@ const Children = () => {
   variant="standard"
 />
 <TextField
+value={parentId}
+onChange={(e)=>setParentId(e.target.value)}
+helperText={parentIdError? parentIdError : ""}
+error={parentIdError}
 autoFocus
 margin="dense"
 id="parentId"
-label="Parent Id"
+label="Parent Phone Number"
 type="text"
 fullWidth
 variant="standard"
 />
 <TextField
+value={classId}
+onChange={(e)=>setClassId(e.target.value)}
+helperText={classIdError? classIdError : ""}
+error={classIdError}
 autoFocus
 select
 margin="dense"
@@ -135,7 +270,11 @@ variant="standard"
   </DialogContent>
   <DialogActions>
     <Button onClick={handleClose}>Cancel</Button>
-    <Button onClick={handleClose}>Submit</Button>
+    {
+      addStudent.loading?"Loading":
+      <Button onClick={(e)=>handleSubmit(e)}>Submit</Button>
+    }
+    
   </DialogActions>
 </Dialog>
     <TableContainer component={Paper} className="teacherTable">
